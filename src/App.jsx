533c6e39614a7ff000477e70bff60f359059429c
Barbara Soares <barbarasamela90@gmail.com>
2023-12-19 14:26:22 -0300

    feat: aplicada formatação em todos os arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,26 @@
-import { Header } from './components/Header';
-import {Post} from './components/Post';
-import { Sidebar } from './components/Sidebar';
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
-import styles from './App.module.css';
+import styles from "./App.module.css";
 
-import './global.css';
+import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      
+
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-        <Post 
-          author="Dory Smith" 
-          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut architecto molestiae odit inventore reprehenderit est veritatis iste sed at! Laudantium quisquam labore odit deserunt soluta ut corporis. Sit, fuga quis!"  
-        />
-        <Post 
-          author="Richard Pierre"
-          content="Um novo post muito legal"
-        />
+          <Post
+            author="Dory Smith"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut architecto molestiae odit inventore reprehenderit est veritatis iste sed at! Laudantium quisquam labore odit deserunt soluta ut corporis. Sit, fuga quis!"
+          />
+          <Post author="Richard Pierre" content="Um novo post muito legal" />
         </main>
       </div>
     </div>
-  )
+  );
 }
-
-
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 //import React from 'react';
-import igniteLogo from '../imag/ignite-logo.svg'; 
+import igniteLogo from "../imag/ignite-logo.svg";
 
 export function Header() {
   return (
@@ -7,23 +7,20 @@ export function Header() {
       <img src={igniteLogo} alt="Ignite Logo" style={styles.logo} />
       <strong>Ignite Feed</strong>
     </header>
-  );          
+  );
 }
 
-const styles = {     
-  header: {         
+const styles = {
+  header: {
     background: "#333",
-    height: 80,         
-    display: "flex",        
-    justifyContent: "center",    
-    alignItems: "center",     
-    padding: "1.25rem 0",     
-
+    height: 80,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: "1.25rem 0",
   },
   logo: {
-    marginRight: '1rem',
-    maxHeight: '100%',
-  }
- }
-
- 
+    marginRight: "1rem",
+    maxHeight: "100%",
+  },
+};
